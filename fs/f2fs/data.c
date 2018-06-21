a9aa179ce67585d603c3d37fe63086eaefc8947b
Jaegeuk Kim <jaegeuk@kernel.org>
2018-06-21 11:29:43 -0700

    f2fs: assign REQ_RAHEAD to bio for ->readpages
    As Jens reported, we'd better assign REQ_RAHEAD to bio by the fact that
    ->readpages is called only from read-ahead.
    
    In Documentation/filesystems/vfs.txt,
    
    readpages: called by the VM to read pages associated with the address_space
      	object. This is essentially just a vector version of
      	readpage.  Instead of just one page, several pages are
      	requested.
    	readpages is only used for read-ahead, so read errors are
      	ignored.  If anything goes wrong, feel free to give up.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit f9d75d65b760b9cea889f16106c869caf92f393f)

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -581,7 +581,7 @@ out:
 }
 
 static struct bio *f2fs_grab_read_bio(struct inode *inode, block_t blkaddr,
-				      unsigned int nr_pages, int flags)
+				      unsigned int nr_pages, int flags, unsigned op_flag)
 {
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	struct bio *bio;
@@ -596,7 +596,7 @@ static struct bio *f2fs_grab_read_bio(struct inode *inode, block_t blkaddr,
 		return ERR_PTR(-ENOMEM);
 	f2fs_target_device(sbi, blkaddr, bio);
 	bio->bi_end_io = f2fs_read_end_io;
-	bio_set_op_attrs(bio, REQ_OP_READ, 0);
+	bio_set_op_attrs(bio, REQ_OP_READ, 0, op_flag);
 
 	if (f2fs_encrypted_file(inode) &&
 	    !fscrypt_using_hardware_encryption(inode))
@@ -621,7 +621,7 @@ static struct bio *f2fs_grab_read_bio(struct inode *inode, block_t blkaddr,
 static int f2fs_submit_page_read(struct inode *inode, struct page *page,
 				 block_t blkaddr, int flags)
 {
-	struct bio *bio = f2fs_grab_read_bio(inode, blkaddr, 1, flags);
+	struct bio *bio = f2fs_grab_read_bio(inode, blkaddr, 1, flags, 0);
 
 	if (IS_ERR(bio))
 		return PTR_ERR(bio);
@@ -1485,10 +1485,15 @@ out:
 /*
  * This function was originally taken from fs/mpage.c, and customized for f2fs.
  * Major change was from block_size == page_size in f2fs by default.
+ *
+ * Note that the aops->readpages() function is ONLY used for read-ahead. If
+ * this function ever deviates from doing just read-ahead, it should either
+ * use ->readpage() or do the necessary surgery to decouple ->readpages()
+ * from read-ahead.
  */
 static int f2fs_mpage_readpages(struct address_space *mapping,
 			struct list_head *pages, struct page *page,
-			unsigned nr_pages)
+			unsigned nr_pages, bool is_readahead)
 {
 	struct bio *bio = NULL;
 	sector_t last_block_in_bio = 0;
@@ -1595,7 +1600,8 @@ submit_and_realloc:
 		}
 
 		if (bio == NULL) {
-			bio = f2fs_grab_read_bio(inode, block_nr, nr_pages, 0);
+			bio = f2fs_grab_read_bio(inode, block_nr, nr_pages, 0,
+					is_readahead ? REQ_RAHEAD : 0);
 			if (IS_ERR(bio)) {
 				bio = NULL;
 				goto set_error_page;
@@ -1647,7 +1653,7 @@ static int f2fs_read_data_page(struct file *file, struct page *page)
 	if (f2fs_has_inline_data(inode))
 		ret = f2fs_read_inline_data(inode, page);
 	if (ret == -EAGAIN)
-		ret = f2fs_mpage_readpages(page->mapping, NULL, page, 1);
+		ret = f2fs_mpage_readpages(page->mapping, NULL, page, 1, false);
 	return ret;
 }
 
@@ -1664,7 +1670,7 @@ static int f2fs_read_data_pages(struct file *file,
 	if (f2fs_has_inline_data(inode))
 		return 0;
 
-	return f2fs_mpage_readpages(mapping, pages, NULL, nr_pages);
+	return f2fs_mpage_readpages(mapping, pages, NULL, nr_pages, true);
 }
 
 static int encrypt_one_page(struct f2fs_io_info *fio)
