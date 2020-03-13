5adfac5146601dc01fa63de4176967048b8de5c3
Josh Chasky <jchasky@gmail.com>
2020-03-13 10:08:56 -0400

    Remove upstream usb composite changes
    Revert "usb: gadget: composite: Support more than 500mA MaxPower"
    Revert "usb: gadget: composite: Fix bMaxPower for SuperSpeedPlus"
    
    This reverts commit 6596754b9 and f7a3c4736.
    
    Change-Id: Ia7034ec6c9cf9f781ccad83980262d422ef55fec

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -555,14 +555,12 @@ static u8 encode_bMaxPower(enum usb_device_speed speed,
 
 	if (!val)
 		return 0;
-	if (speed < USB_SPEED_SUPER)
-		return min(val, 500U) / 2;
-	else
-		/*
-		 * USB 3.x supports up to 900mA, but since 900 isn't divisible
-		 * by 8 the integral division will effectively cap to 896mA.
-		 */
-		return min(val, 900U) / 8;
+	switch (speed) {
+	case USB_SPEED_SUPER:
+		return DIV_ROUND_UP(val, 8);
+	default:
+		return DIV_ROUND_UP(val, 2);
+	}
 }
 
 static int config_buf(struct usb_configuration *config,
@@ -980,10 +978,6 @@ static int set_config(struct usb_composite_dev *cdev,
 
 	/* when we return, be sure our power usage is valid */
 	power = c->MaxPower ? c->MaxPower : CONFIG_USB_GADGET_VBUS_DRAW;
-	if (gadget->speed < USB_SPEED_SUPER)
-		power = min(power, 500U);
-	else
-		power = min(power, 900U);
 done:
 	usb_gadget_vbus_draw(gadget, USB_VBUS_DRAW(gadget->speed));
 	if (result >= 0 && cdev->delayed_status)
@@ -2476,7 +2470,7 @@ void composite_resume(struct usb_gadget *gadget)
 	struct usb_function		*f;
 	int				ret;
 	unsigned long			flags;
-	unsigned			maxpower;
+	u16				maxpower;
 
 	/* REVISIT:  should we have config level
 	 * suspend/resume callbacks?
@@ -2507,14 +2501,10 @@ void composite_resume(struct usb_gadget *gadget)
 				f->resume(f);
 		}
 
-		maxpower = cdev->config->MaxPower ?
-			cdev->config->MaxPower : CONFIG_USB_GADGET_VBUS_DRAW;
-		if (gadget->speed < USB_SPEED_SUPER)
-			maxpower = min(maxpower, 500U);
-		else
-			maxpower = min(maxpower, 900U);
+		maxpower = cdev->config->MaxPower;
 
-		usb_gadget_vbus_draw(gadget, maxpower);
+		usb_gadget_vbus_draw(gadget, maxpower ?
+			maxpower : CONFIG_USB_GADGET_VBUS_DRAW);
 	}
 
 	spin_unlock_irqrestore(&cdev->lock, flags);
